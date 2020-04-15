bd6a17015a1c951a444af4fb5097f50d6f135ceb
Mario Gamal <m.gamal@tap.company>
2020-04-15 12:09:28 +0300

    add CardScheme

---
+++ app/src/main/java/company/tap/commondatamodelskit_android/CardScheme.kt
@@ -0,0 +1,62 @@
+package company.tap.commondatamodelskit_android
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ *
+ * Created by Mario Gamal on 4/15/20
+ * Copyright © 2020 Tap Payments. All rights reserved.
+ *
+ */
+enum class CardScheme {
+    /**
+     * Knet card scheme.
+     */
+    @SerializedName("KNET")
+    KNET,
+    /**
+     * Visa card scheme.
+     */
+    @SerializedName("VISA")
+    VISA,
+    /**
+     * Mastercard card scheme.
+     */
+    @SerializedName("MASTERCARD")
+    MASTERCARD,
+    /**
+     * American express card scheme.
+     */
+    @SerializedName("AMERICAN_EXPRESS")
+    AMERICAN_EXPRESS,
+    /**
+     * Mada card scheme.
+     */
+    @SerializedName("MADA")
+    MADA,
+    /**
+     * Benefit card scheme.
+     */
+    @SerializedName("BENEFIT")
+    BENEFIT,
+    /**
+     * Sadad card scheme.
+     */
+    @SerializedName("SADAD_ACCOUNT")
+    SADAD,
+    /**
+     * Fawry card scheme.
+     */
+    @SerializedName("FAWRY")
+    FAWRY,
+    /**
+     * Naps card scheme.
+     */
+    @SerializedName("NAPS")
+    NAPS,
+    /**
+     * Oman net card scheme.
+     */
+    @SerializedName("OMAN_NET")
+    OMAN_NET;
+}
