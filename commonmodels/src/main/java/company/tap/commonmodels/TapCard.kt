be111ea2c7cc901bba81df876e839cb724e3bb53
Mario Gamal <m.gamal@tap.company>
2020-04-15 17:34:53 +0300

    make card nullabke

--- commonmodels/src/main/java/company/tap/commonmodels/TapCard.kt
+++ commonmodels/src/main/java/company/tap/commonmodels/TapCard.kt
@@ -7,21 +7,21 @@ package company.tap.commonmodels
  *
  */
 data class TapCard(
-    var id: String,
-    var objecT: String,
-    var firstSix: String,
-    var lastFour: String,
-    var expMonth: String,
-    var expYear: String,
-    var brand: CardBrand,
-    var name: String,
-    var bin: String,
-    var expiry: String,
-    var currency: TapCurrency,
-    var scheme: CardScheme,
-    var supportedCurrencies: List<TapCurrency>,
-    var orderBy: String,
-    var paymentMethodId: String,
-    var fingerprint: String,
-    var image: String
+    var id: String? = null,
+    var cardObject: String? = null,
+    var firstSix: String? = null,
+    var lastFour: String? = null,
+    var expMonth: String? = null,
+    var expYear: String? = null,
+    var brand: CardBrand? = null,
+    var name: String? = null,
+    var bin: String? = null,
+    var expiry: String? = null,
+    var currency: TapCurrency? = null,
+    var scheme: CardScheme? = null,
+    var supportedCurrencies: List<TapCurrency>? = null,
+    var orderBy: String? = null,
+    var paymentMethodId: String? = null,
+    var fingerprint: String? = null,
+    var image: String? = null
 )
